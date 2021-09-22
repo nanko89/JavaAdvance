5ceb37c81fc61c3f467a6a10628f316713df86ac
nanko89 <natali.v.angelova@gmail.com>
2021-09-22 17:42:04 +0300

    Java Advance Lab

---
+++ Defining Classes/Lab/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+            Car car = new Car();
+            car.setBrand(tokens[0]);
+            car.setModel(tokens[1]);
+            car.setHorsePower(Integer.parseInt(tokens[2]));
+
+        System.out.printf("The car is: %s %s - %d HP.%n", car.getBrand(),car.getModel(),car.getHorsePower());
+        }
+
+
+    }
+}
