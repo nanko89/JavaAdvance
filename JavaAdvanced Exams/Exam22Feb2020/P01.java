8febcb4d720d968043db97854802a7f9156ad704
nanko89 <natali.v.angelova@gmail.com>
2021-10-21 18:49:32 +0300

    Java Advanced Exam

--- JavaAdvanced Exams/Exam22Feb2020/P01.java
+++ JavaAdvanced Exams/Exam22Feb2020/P01.java
@@ -8,23 +8,26 @@ import java.util.stream.Collectors;
 public class P01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayDeque<Integer> firstBox = Arrays.stream(scanner.nextLine().split("\\s+"))
+        ArrayDeque<Integer> firstBox = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toCollection(ArrayDeque::new));
 
         ArrayDeque<Integer> secondBox = new ArrayDeque<>();
-                Arrays.stream(scanner.nextLine().split("\\s+"))
+        Arrays.stream(scanner.nextLine().split("\\s+"))
                 .map(Integer::parseInt)
                 .forEach(secondBox::push);
-        int claimed = 0;
+
+        int collection = 0;
+
         while (!firstBox.isEmpty() && !secondBox.isEmpty()) {
             int first = firstBox.peek();
             int second = secondBox.pop();
-
             int result = first + second;
+
             if (result % 2 == 0){
-                claimed += result;
-                firstBox.pollFirst();
+                collection += result;
+                firstBox.poll();
 
             }else {
                 firstBox.offer(second);
@@ -32,14 +35,15 @@ public class P01 {
         }
         if (firstBox.isEmpty()){
             System.out.println("First lootbox is empty");
-        }else {
+        }
+        if (secondBox.isEmpty()){
             System.out.println("Second lootbox is empty");
         }
-        if (claimed >= 100){
-            System.out.println("Your loot was epic! Value: " + claimed);
-        }else {
-            System.out.println("Your loot was poor... Value: " + claimed);
-        }
 
+        if (collection >= 100){
+            System.out.println("Your loot was epic! Value: " + collection);
+        }else {
+            System.out.println("Your loot was poor... Value: " + collection);
+        }
     }
 }
